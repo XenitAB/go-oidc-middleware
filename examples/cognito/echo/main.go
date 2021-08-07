6394864bcaeabb4bdd308ea69e1e7de43d7ff5ad
Simon Gottschlag <simon.gottschlag@xenit.se>
2021-08-08 01:00:46 +0200

    Add Cognito example (#16)

---
+++ examples/cognito/echo/main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"examples/shared"
+	"fmt"
+	"os"
+
+	"github.com/xenitab/go-oidc-middleware/oidcechojwt"
+)
+
+func main() {
+	cfg, err := shared.NewCognitoConfig()
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "failed to load config: %v\n", err)
+		os.Exit(1)
+	}
+
+	err = run(cfg)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "application returned error: %v\n", err)
+		os.Exit(1)
+	}
+}
+
+func run(cfg shared.CognitoConfig) error {
+	parseToken := oidcechojwt.New(&oidcechojwt.Options{
+		Issuer:                     cfg.Issuer,
+		FallbackSignatureAlgorithm: cfg.FallbackSignatureAlgorithm,
+		RequiredClaims: map[string]interface{}{
+			"client_id": cfg.ClientID,
+		},
+	})
+
+	return shared.RunEchoJWT(parseToken, cfg.Address, cfg.Port)
+}
