39b1fdd1661d6a78180adface165ed37b394506a
Simon Gottschlag <simon.gottschlag@xenit.se>
2021-08-11 22:40:23 +0200

    generalize token string

---
+++ options/tokenstring.go
@@ -0,0 +1,40 @@
+package options
+
+// TokenStringOptions handles the settings for how to extract the token from a request.
+type TokenStringOptions struct {
+	// HeaderName is the name of the header.
+	// Default: "Authorization"
+	HeaderName string
+
+	// Delimiter is the delimiter between the token type and the token.
+	// Default: " " (single space)
+	Delimiter string
+
+	// TokenType is the type of token that is sent.
+	// Default: "Bearer"
+	TokenType string
+}
+
+// TokenStringOption returns a function that modifies a TokenStringOptions pointer.
+type TokenStringOption func(*TokenStringOptions)
+
+// WithTokenStringHeaderName sets the HeaderName parameter for a TokenStringOptions pointer.
+func WithTokenStringHeaderName(opt string) TokenStringOption {
+	return func(opts *TokenStringOptions) {
+		opts.HeaderName = opt
+	}
+}
+
+// WithTokenStringDelimiter sets the Delimiter parameter for a TokenStringOptions pointer.
+func WithTokenStringDelimiter(opt string) TokenStringOption {
+	return func(opts *TokenStringOptions) {
+		opts.Delimiter = opt
+	}
+}
+
+// WithTokenStringTokenType sets the TokenType for a TokenStringOptions pointer.
+func WithTokenStringTokenType(opt string) TokenStringOption {
+	return func(opts *TokenStringOptions) {
+		opts.TokenType = opt
+	}
+}
