61e456df485da00c2f507536958ba7bb77b95035
Simon Gottschlag <simon.gottschlag@xenit.se>
2023-02-20 22:05:24 +0100

    Add tokenCache

--- internal/oidc/opaque.go
+++ internal/oidc/opaque.go
@@ -6,51 +6,111 @@ import (
 	"fmt"
 	"io"
 	"net/http"
+	"sync"
 	"time"
 
 	"github.com/xenitab/go-oidc-middleware/options"
 )
 
+type opaqueToken[T any] struct {
+	claims  T
+	created time.Time
+}
+
+type opaqueTokenCache[T any] struct {
+	timeToLive time.Duration
+	tokens     map[string]opaqueToken[T]
+	mu         sync.RWMutex
+}
+
+func (c *opaqueTokenCache[T]) set(key string, value T) {
+	c.mu.Lock()
+	c.tokens[key] = opaqueToken[T]{
+		claims:  value,
+		created: time.Now(),
+	}
+	c.mu.Unlock()
+}
+
+func (c *opaqueTokenCache[T]) get(key string) (T, bool) {
+	c.mu.RLock()
+	now := time.Now()
+	for k, v := range c.tokens {
+		tokenExpiration := v.created.Add(c.timeToLive)
+		if tokenExpiration.After(now) {
+			delete(c.tokens, k)
+		}
+	}
+	value, ok := c.tokens[key]
+	c.mu.RUnlock()
+	return value.claims, ok
+}
+
 type opaqueHandler[T any] struct {
-	issuer                string
-	discoveryUri          string
-	discoveryFetchTimeout time.Duration
-	userinfoEndpoint      string
-	httpClient            *http.Client
-	claimsValidationFn    options.ClaimsValidationFn[T]
+	introspectionUri          string
+	introspectionFetchTimeout time.Duration
+	tokenCache                *opaqueTokenCache[T]
+	tokenCacheEnabled         bool
+	httpClient                *http.Client
+	claimsValidationFn        options.ClaimsValidationFn[T]
 }
 
 func NewOpaqueHandler[T any](claimsValidationFn options.ClaimsValidationFn[T], setters ...options.OpaqueOption) (*opaqueHandler[T], error) {
 	opts := options.NewOpaque(setters...)
 	h := &opaqueHandler[T]{
-		issuer:                opts.Issuer,
-		discoveryUri:          opts.DiscoveryUri,
-		discoveryFetchTimeout: opts.DiscoveryFetchTimeout,
-		httpClient:            opts.HttpClient,
-		claimsValidationFn:    claimsValidationFn,
+		introspectionUri:          opts.IntrospectionUri,
+		introspectionFetchTimeout: opts.IntrospectionFetchTimeout,
+		tokenCache: &opaqueTokenCache[T]{
+			timeToLive: opts.TokenCacheTimeToLive,
+		},
+		tokenCacheEnabled:  opts.TokenCacheTimeToLive != 0,
+		httpClient:         opts.HttpClient,
+		claimsValidationFn: claimsValidationFn,
 	}
 
-	if h.issuer == "" {
-		return nil, fmt.Errorf("issuer is empty")
-	}
-
-	if h.discoveryUri == "" {
-		h.discoveryUri = GetDiscoveryUriFromIssuer(h.issuer)
-	}
-
-	metadata, err := getOidcMetadataFromDiscoveryUri(h.httpClient, h.discoveryUri, h.discoveryFetchTimeout)
+	err := h.populateIntrospectionUri(opts.Issuer, opts.DiscoveryUri, opts.DiscoveryFetchTimeout)
 	if err != nil {
-		return nil, fmt.Errorf("unable to fetch userinfo_endpoint from discoveryUri (%s): %w", h.discoveryUri, err)
+		return nil, err
 	}
-	if metadata.UserinfoEndpoint == "" {
-		return nil, fmt.Errorf("UserinfoEndpoint is empty")
-	}
-	h.userinfoEndpoint = metadata.UserinfoEndpoint
 
 	return h, nil
 }
 
+func (h *opaqueHandler[T]) populateIntrospectionUri(issuerUri string, discoveryUri string, discoveryFetchTimeout time.Duration) error {
+	if h.introspectionUri != "" {
+		return nil
+	}
+
+	if issuerUri == "" && discoveryUri == "" {
+		return fmt.Errorf("issuer and discoveryUri are both empty")
+	}
+
+	if discoveryUri == "" {
+		discoveryUri = GetDiscoveryUriFromIssuer(issuerUri)
+	}
+
+	metadata, err := getOidcMetadataFromDiscoveryUri(h.httpClient, discoveryUri, discoveryFetchTimeout)
+	if err != nil {
+		return fmt.Errorf("unable to fetch userinfo_endpoint from discoveryUri (%s): %w", discoveryUri, err)
+	}
+
+	if metadata.UserinfoEndpoint == "" {
+		return fmt.Errorf("UserinfoEndpoint is empty")
+	}
+
+	h.introspectionUri = metadata.UserinfoEndpoint
+
+	return nil
+}
+
 func (h *opaqueHandler[T]) ParseToken(ctx context.Context, tokenString string) (T, error) {
+	if h.tokenCacheEnabled {
+		claims, ok := h.tokenCache.get(tokenString)
+		if ok {
+			return claims, nil
+		}
+	}
+
 	claims, err := h.introspectToken(ctx, tokenString)
 	if err != nil {
 		return *new(T), fmt.Errorf("unable to introspect the token: %w", err)
@@ -59,6 +119,10 @@ func (h *opaqueHandler[T]) ParseToken(ctx context.Context, tokenString string) (
 	err = h.validateClaims(&claims)
 	if err != nil {
 		return *new(T), fmt.Errorf("claims validation returned an error: %w", err)
+	}
+
+	if h.tokenCacheEnabled {
+		h.tokenCache.set(tokenString, claims)
 	}
 
 	return claims, nil
@@ -73,7 +137,10 @@ func (h *opaqueHandler[T]) validateClaims(claims *T) error {
 }
 
 func (h *opaqueHandler[T]) introspectToken(ctx context.Context, tokenString string) (T, error) {
-	req, err := http.NewRequestWithContext(ctx, http.MethodGet, h.userinfoEndpoint, http.NoBody)
+	ctx, cancel := context.WithTimeout(ctx, h.introspectionFetchTimeout)
+	defer cancel()
+
+	req, err := http.NewRequestWithContext(ctx, http.MethodGet, h.introspectionUri, http.NoBody)
 	if err != nil {
 		return *new(T), err
 	}
--- options/opaque_options.go
+++ options/opaque_options.go
@@ -7,23 +7,27 @@ import (
 
 // OpaqueOptions defines the options for OIDC Middleware.
 type OpaqueOptions struct {
-	Issuer                string
-	DiscoveryUri          string
-	DiscoveryFetchTimeout time.Duration
-	HttpClient            *http.Client
-	TokenString           [][]TokenStringOption
-	ClaimsContextKeyName  ClaimsContextKeyName
-	ErrorHandler          ErrorHandler
+	Issuer                    string
+	DiscoveryUri              string
+	DiscoveryFetchTimeout     time.Duration
+	IntrospectionUri          string
+	IntrospectionFetchTimeout time.Duration
+	TokenCacheTimeToLive      time.Duration
+	HttpClient                *http.Client
+	TokenString               [][]TokenStringOption
+	ClaimsContextKeyName      ClaimsContextKeyName
+	ErrorHandler              ErrorHandler
 }
 
-// New takes Option setters and returns an Options pointer.
+// NewOpaque takes Option setters and returns an OpaqueOptions pointer.
 // Mainly used by the internal functions and most likely not
 // needed by any external application using this library.
 func NewOpaque(setters ...OpaqueOption) *OpaqueOptions {
 	opts := &OpaqueOptions{
-		DiscoveryFetchTimeout: 5 * time.Second,
-		HttpClient:            http.DefaultClient,
-		ClaimsContextKeyName:  DefaultClaimsContextKeyName,
+		DiscoveryFetchTimeout:     5 * time.Second,
+		IntrospectionFetchTimeout: 5 * time.Second,
+		HttpClient:                http.DefaultClient,
+		ClaimsContextKeyName:      DefaultClaimsContextKeyName,
 	}
 
 	for _, setter := range setters {
@@ -33,7 +37,7 @@ func NewOpaque(setters ...OpaqueOption) *OpaqueOptions {
 	return opts
 }
 
-// Option returns a function that modifies an Options pointer.
+// OpaqueOption returns a function that modifies an OpaqueOptions pointer.
 type OpaqueOption func(*OpaqueOptions)
 
 // WithOpaqueIssuer sets the Issuer parameter for Options.
@@ -44,8 +48,8 @@ func WithOpaqueIssuer(opt string) OpaqueOption {
 	}
 }
 
-// WithOpaqueDiscoveryUri sets the Issuer parameter for an Options pointer.
-// DiscoveryUri is where the `jwks_uri` will be grabbed
+// WithOpaqueDiscoveryUri sets the Issuer parameter for an OpaqueOptions pointer.
+// DiscoveryUri is where the `userinfo_endpoint` will be grabbed
 // Defaults to `fmt.Sprintf("%s/.well-known/openid-configuration", strings.TrimSuffix(issuer, "/"))`
 func WithOpaqueDiscoveryUri(opt string) OpaqueOption {
 	return func(opts *OpaqueOptions) {
@@ -53,7 +57,7 @@ func WithOpaqueDiscoveryUri(opt string) OpaqueOption {
 	}
 }
 
-// WithOpaqueDiscoveryFetchTimeout sets the DiscoveryFetchTimeout parameter for an Options pointer.
+// WithOpaqueDiscoveryFetchTimeout sets the DiscoveryFetchTimeout parameter for an OpaqueOptions pointer.
 // DiscoveryFetchTimeout sets the context timeout when downloading the discovery metadata
 // Defaults to 5 seconds
 func WithOpaqueDiscoveryFetchTimeout(opt time.Duration) OpaqueOption {
@@ -62,7 +66,34 @@ func WithOpaqueDiscoveryFetchTimeout(opt time.Duration) OpaqueOption {
 	}
 }
 
-// WithOpaqueHttpClient sets the HttpClient parameter for an Options pointer.
+// WithOpaqueIntrospectionUri sets the Issuer parameter for an OpaqueOptions pointer.
+// IntrospectionUri is where the `userinfo_endpoint` from the discovery metadata is pointing.
+// Defaults to what's fetched from the discovery metadata, but if defined here will be overwritten.
+func WithOpaqueIntrospectionUri(opt string) OpaqueOption {
+	return func(opts *OpaqueOptions) {
+		opts.IntrospectionUri = opt
+	}
+}
+
+// WithOpaqueIntrospectionFetchTimeout sets the IntrospectionFetchTimeout parameter for an OpaqueOptions pointer.
+// IntrospectionFetchTimeout sets the context timeout when sending the introspection request
+// Defaults to 5 seconds
+func WithOpaqueIntrospectionFetchTimeout(opt time.Duration) OpaqueOption {
+	return func(opts *OpaqueOptions) {
+		opts.IntrospectionFetchTimeout = opt
+	}
+}
+
+// WithOpaqueTokenCacheTimeToLive sets the TokenCacheTimeToLive parameter for an OpaqueOptions pointer.
+// TokenCacheTimeToLive sets the token cache ttl (time to live)
+// Defaults to disabled (0s)
+func WithOpaqueTokenCacheTimeToLive(opt time.Duration) OpaqueOption {
+	return func(opts *OpaqueOptions) {
+		opts.IntrospectionFetchTimeout = opt
+	}
+}
+
+// WithOpaqueHttpClient sets the HttpClient parameter for an OpaqueOptions pointer.
 // HttpClient takes a *http.Client for external calls
 // Defaults to http.DefaultClient
 func WithOpaqueHttpClient(opt *http.Client) OpaqueOption {
@@ -71,7 +102,7 @@ func WithOpaqueHttpClient(opt *http.Client) OpaqueOption {
 	}
 }
 
-// WithOpaqueTokenString sets the TokenString parameter for an Options pointer.
+// WithOpaqueTokenString sets the TokenString parameter for an OpaqueOptions pointer.
 // TokenString makes it possible to configure how the JWT token should be extracted from
 // an http header. Not supported by Echo JWT and will be ignored if used by it.
 // Defaults to: 'Authorization: Bearer JWT'
@@ -84,7 +115,7 @@ func WithOpaqueTokenString(setters ...TokenStringOption) OpaqueOption {
 	}
 }
 
-// WithOpaqueClaimsContextKeyName sets the ClaimsContextKeyName parameter for an Options pointer.
+// WithOpaqueClaimsContextKeyName sets the ClaimsContextKeyName parameter for an OpaqueOptions pointer.
 // ClaimsContextKeyName is the name of key that will be used to pass claims using request context.
 // Not supported by Echo JWT and will be ignored if used by it.
 //
@@ -103,7 +134,7 @@ func WithOpaqueClaimsContextKeyName(opt string) OpaqueOption {
 	}
 }
 
-// WithOpaqueErrorHandler sets the ErrorHandler parameter for an Options pointer.
+// WithOpaqueErrorHandler sets the ErrorHandler parameter for an OpaqueOptions pointer.
 // You can pass a function to run custom logic on errors, logging as an example.
 // Defaults to nil
 func WithOpaqueErrorHandler(opt ErrorHandler) OpaqueOption {
